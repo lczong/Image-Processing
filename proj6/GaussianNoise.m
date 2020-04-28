d1e02afc48607e9055094d22569db7c432cff643
Zolich666 <Licheng.Zong@outlook.com>
2020-04-29 00:02:26 +0800

    all codes work

--- proj6/GaussianNoise.m
+++ proj6/GaussianNoise.m
@@ -1,19 +1,19 @@
-[img,map]=imread('images/lena.bmp');
+% 第一题
+clc;clear;
+img=imread('images/lena.bmp');
 figure(1);
 subplot(2,2,1);
 imshow(img);
 title('原始图像','Fontsize',24);
-img1=imnoise(img,'gaussian',0,0.01);
-subplot(2,2,2);
-imshow(img1,map);
+img1=imnoise(img,'gaussian',0,0.05);
+subplot(2,2,2);imshow(img1);
 title('加入Gaussian噪声图像','Fontsize',24);
 
-h1=fspecial('average',[3 3]); % 建立一个3*3模板的高斯滤波器
+h1=fspecial('average',[5 5]); % 建立一个5*5模板的均值滤波器
 img2=imfilter(img1,h1,'conv');
-subplot(2,2,3);
-imshow(img2,map);
+subplot(2,2,3);imshow(img2);
 title('均值滤波后图像','Fontsize',24);
 
-img3=medfilt2(img1,[3 3]); 
-subplot(2,2,4),imshow(img1,map);
+img3=medfilt2(img1,[5 5]); % 5*5模板的中值滤波
+subplot(2,2,4);imshow(img3);
 title('中值滤波后图像','Fontsize',24);
--- proj6/SaltPepperNoise.m
+++ proj6/SaltPepperNoise.m
@@ -1,19 +1,21 @@
+% 第二题
+clc;clear;
 img=imread('images/lena.bmp');
 figure(1);
 subplot(2,2,1);
 imshow(img);
-title('原始图像','fontsize',15);
-img2=imnoise(img,'salt & pepper',0.1);
+title('原始图像','fontsize',24);
+img1=imnoise(img,'salt & pepper',0.1);
 subplot(2,2,2);
-imshow(img2);
-title('加入椒盐噪声图像','fontsize',15);
+imshow(img1);
+title('加入椒盐噪声图像','fontsize',24);
 
-h1=fspecial('gaussian',[3 3],1);%建立一个3*3模板的高斯滤波器
+h1=fspecial('average',[3 3]); % 建立一个3*3模板的均值滤波器
 img2=imfilter(img1,h1,'conv');
 subplot(2,2,3);
-imshow(img2,map);
-title('Gaussian滤波后图像','Fontsize',24);
+imshow(img2);
+title('均值滤波后图像','Fontsize',24);
 
-img3=medfilt2(img1,[5 5]); 
-subplot(2,2,4),imshow(img1,map);
+img3=medfilt2(img1,[5 5]); % 5*5模板的中值滤波
+subplot(2,2,4),imshow(img3);
 title('中值滤波后图像','Fontsize',24);
